caa880c126ef945cb264a45c887c818e0b76d7ca
KoinoEngineering <email@koino.engineering>
2020-12-30 00:49:07 +0900

    #1 fix errors

--- back/back/urls.py
+++ back/back/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart import views
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
