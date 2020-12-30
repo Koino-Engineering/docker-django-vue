4c13e7cf5fe912316c9d94c73ddafe47382a3fbf
KoinoEngineering <email@koino.engineering>
2020-12-30 17:42:30 +0900

    #4 add deleted_at

--- back/wiki/models.py
+++ back/wiki/models.py
@@ -6,3 +6,6 @@ from utils.models import BaseModel
 class Article(BaseModel):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
+
+    class Meta:
+        unique_together = ('title', 'deleted_at')
